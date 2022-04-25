012bad9a69c6899feb3e3156281305996a1493b4
appletime81 <itrimsl8696@gmail.com>
2022-04-25 20:00:53 +0800

    Update

--- asrock_server_init.py
+++ asrock_server_init.py
@@ -28,21 +28,14 @@ class App:
         self.server_host = input(f"{bcolors.OKBLUE}Server host: {bcolors.ENDC}")
 
         # cd root path
-        root_path = input(f"{bcolors.OKBLUE}Go to {bcolors.ENDC}{bcolors.WARNING}{ROOT_DIR[self.server_host]}{bcolors.ENDC} {bcolors.OKBLUE}? (y/n){bcolors.ENDC}")
+        root_path = input(
+            f"{bcolors.OKBLUE}Go to {bcolors.ENDC}{bcolors.WARNING}{ROOT_DIR[self.server_host]}{bcolors.ENDC} {bcolors.OKBLUE}? (y/n){bcolors.ENDC}"
+        )
         if root_path == "y":
-
-            try:
-                os.chdir(ROOT_DIR[self.server_host])
-            except:
-                pass
-
+            os.chdir(ROOT_DIR[self.server_host])
         else:
             root_path = input(f"{bcolors.OKBLUE}Please input root path: {bcolors.ENDC}")
-
-            try:
-                os.chdir(root_path)
-            except:
-                pass
+            os.chdir(root_path)
 
         # run run_rtp_setting
         self.run_rtp_setting()
@@ -52,34 +45,27 @@ class App:
 
     def run_rtp_setting(self):
         if self.server_host == "50":
-            print(f"{bcolors.OKBLUE}Start running {bcolors.ENDC}{bcolors.WARNING}do_ptp.sh{bcolors.ENDC}")
-
-            try:
-                os.system("./do_ptp.sh")
-            except:
-                pass
-
+            print(
+                f"{bcolors.OKBLUE}Start running {bcolors.ENDC}{bcolors.WARNING}do_ptp.sh{bcolors.ENDC}"
+            )
+            os.system("./do_ptp.sh")
             print(f"{bcolors.WARNING}Running done !{bcolors.ENDC}")
         elif self.server_host == "25":
-            print(f"{bcolors.OKBLUE}Start running {bcolors.ENDC}{bcolors.WARNING}./setup_ota_e2e ptp_old{bcolors.ENDC}")
-
-            try:
-                os.system("./setup_ota_e2e ptp_old")
-            except:
-                pass
-
+            print(
+                f"{bcolors.OKBLUE}Start running {bcolors.ENDC}{bcolors.WARNING}./setup_ota_e2e ptp_old{bcolors.ENDC}"
+            )
+            os.system("./setup_ota_e2e ptp_old")
             print(f"{bcolors.WARNING}Running done !{bcolors.ENDC}")
 
     def gnbctl_init(self):
         for item in INIT_LIST:
-            print(f"{bcolors.OKBLUE}Start Init {bcolors.ENDC}{bcolors.WARNING}{item}{bcolors.ENDC}")
-
-            try:
-                os.system(f"./gnbctl {item} init")
-            except:
-                pass
-
-            print(f"{bcolors.WARNING}{item}{bcolors.ENDC} {bcolors.OKBLUE}init done !{bcolors.ENDC}")
+            print(
+                f"{bcolors.OKBLUE}Start Init {bcolors.ENDC}{bcolors.WARNING}{item}{bcolors.ENDC}"
+            )
+            os.system(f"./gnbctl {item} init")
+            print(
+                f"{bcolors.WARNING}{item}{bcolors.ENDC} {bcolors.OKBLUE}init done !{bcolors.ENDC}"
+            )
 
 
 if __name__ == "__main__":
